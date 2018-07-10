11681fbfc20a05bee462d691d892a89cbad97326
huangjinrong <huangjinrong@huya.com>
2018-07-10 21:17:11 +0800

    add config server

--- config-client/src/main/java/cn/hjr/ConfigClientApplication.java
+++ config-client/src/main/java/cn/hjr/ConfigClientApplication.java
@@ -3,9 +3,11 @@ package cn.hjr;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class ConfigClientApplication {
